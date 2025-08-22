76543ffa411d6e042863f7b6c7c32f2810f719c3
Sarthak Hatwar <sarthakhatwar@Sarthaks-MacBook-Air.local>
2025-08-22 00:14:59 -0500

    finalchange2

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  basePath: isProd ? "/sarthak-final" : "",
-  assetPrefix: isProd ? "/sarthak-final/" : "",
+  basePath: isProd ? "/Portfolio1" : "",
+  assetPrefix: isProd ? "/Portfolio1/" : "",
   output: "export",
   trailingSlash: true,
   images: {
